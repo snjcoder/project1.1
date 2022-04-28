65059065f9cc33b7c7ff9ad92bd14c1058b6f17d
neetha <neethacohort@gmail.com>
2022-04-28 16:08:22 -0600

    added home page

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+      <img src="./images/1.jpg" height={700} width={1600} />
+      <div id="hero">
+        <h1>Connecting Local Farmer's with Local Communities</h1>
+      </div>
+    </div>
+  );
+};
+
+export default home;
