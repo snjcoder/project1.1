3783f93b76478e10dd35dcf1e63352429e78df2a
neetha <neethacohort@gmail.com>
2022-05-26 15:29:59 -0600

    mui package

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Login from "./pages/Login";
 import AboutUs from "./component/Aboutus/AboutUs";
+import Navigation from "./component/Navigation";
+import SignUpForm from "./component/SignUp";
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUpForm />} />
       </Routes>
     </Router>
   );
